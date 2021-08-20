fa3195551976533aacba4846938f8f9196af7e1b
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 21:14:03 +0300

    Create Chart.cs

---
+++ Chart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series["Book"].Points.AddXY("Akdeniz", 5);
+            chart1.Series["Book"].Points.AddXY("Ege", 10);
+            chart1.Series["Book"].Points.AddXY("Marmara", 35);
+            chart1.Series["Book"].Points.AddXY("Karadeniz", 4);
+            chart1.Series["Book"].Points.AddXY("Anadolu", 2);
+        }
+    } 
+}
