070cb56b308861cd19fa4d6081bd3d0e6a8dc981
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 19:49:15 +0300

    Create MenuStrip.cs

---
+++ MenuStrip.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.AliceBlue;
+        }
+
+        private void orangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void aboutWeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This project made by Aysegul Yilmaz", "information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void purpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.MediumPurple;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
